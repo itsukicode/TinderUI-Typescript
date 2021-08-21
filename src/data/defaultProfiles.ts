6508bb3ba6a151b8311f0c36ce0abf55dd9b85b7
tommytommychopper <t.itsuki928@gmail.com>
2021-08-21 13:08:07 -0700

    💄style: カードの枚数やカード内の文字数変更

---
+++ src/data/defaultProfiles.ts
@@ -0,0 +1,26 @@
+export const defaultProfiles = [
+  {
+    name: 'Mia',
+    age: '28',
+    imageSrc: '',
+    bio: 'Looking for some positive moments, adventures and fun 🐸  Feel free to message me.',
+  },
+  {
+    name: 'Zoe',
+    age: '20',
+    imageSrc: '',
+    bio: 'Looking for some positive moments, adventures and fun 🦄  Feel free to message me.',
+  },
+  {
+    name: 'Isabella',
+    age: '25',
+    imageSrc: '',
+    bio: 'Looking for some positive moments, adventures and fun 🐙  Feel free to message me. Looking for some positive moments, adventures and fun 🐙  Feel free to message me. Feel free to message me. Looking for some positive moments, adventures and fun 🐙  Feel free to message me.',
+  },
+  {
+    name: 'Becky',
+    age: '22',
+    imageSrc: '',
+    bio: 'Looking for some positive moments, adventures and fun 😁  Feel free to message me.',
+  }
+]
